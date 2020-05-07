d0c911c4c48908e0a43460d1c74d443db7135ddd
the-gero <47275336+the-gero@users.noreply.github.com>
2020-05-07 13:06:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.gps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    Button getLocationBtn;
+    TextView locationText;
+
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
+        locationText = (TextView)findViewById(R.id.locationText);
+
+        getLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+    }
+
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            Log.v("btn","Working");
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
+
+        getLocation();
+        Toast.makeText(getApplicationContext(), "Your Location Latitude :"+location.getLatitude()+"Longitude:"+location.getLongitude(), Toast.LENGTH_LONG).show();
+        Log.v("btn","Working"+location.getLatitude()+"Longitude:"+location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Toast.makeText(getApplicationContext(), "Your Location", Toast.LENGTH_LONG).show();
+        getLocation();
+        Toast.makeText(getApplicationContext(), "Your Location showing", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+}
